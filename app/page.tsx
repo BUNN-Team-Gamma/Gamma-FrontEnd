98b5adc50572223858073b5bda21666d9d4cbd98
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2024-04-16 15:44:40 +0100

    moved navbar and footed to home page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
+import Navbar from "@/components/navbar";
 import AIServices from "@/components/ui/Home/AIServices";
 import Contact from "@/components/ui/Home/Contact";
 import Demo from "@/components/ui/Home/Demo";
 import Hero from "@/components/ui/Home/Hero";
 import Reviews from "@/components/ui/Home/Reviews";
+import Footer from "@/components/ui/footer";
 
 export default function Home() {
   return (
-    <main>
-      <Hero/>
-      <AIServices/>
-      <Demo/>
-      <Reviews/>
-      <Contact/>
-    </main>
+    <>
+      <Navbar/>
+      <main>
+        <Hero/>
+        <AIServices/>
+        <Demo/>
+        <Reviews/>
+        <Contact/>
+      </main>
+      <Footer/>
+    </>
   );
 }
