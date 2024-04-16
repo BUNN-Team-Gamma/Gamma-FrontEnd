e5a8c8cba44183912e89b05ca0c01ec1098664e8
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2024-04-16 14:25:20 +0100

    implemented landing page

--- components/ui/Home/ContactUsForm.tsx
+++ components/ui/Home/ContactUsForm.tsx
@@ -22,7 +22,7 @@ export default function ContactUsForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='lg:text-sm 2xl:text-base mt-2 flex flex-col gap-3 lg:gap-1.5 2xl:gap-3'>
+    <form onSubmit={handleSubmit(onSubmit)} className='text-sm md:text-base lg:text-sm 2xl:text-base mt-2 flex flex-col gap-3 lg:gap-1.5 2xl:gap-3'>
       <div className='flex flex-col gap-1'>
         <label htmlFor="name" className='text-white'>Name: </label>
         <input
