5c391fccadb21abcedd10bbd28830e8a42b628df
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2024-04-18 16:05:22 +0100

    implemented flashcard feature

--- components/ui/myFiles/listView.tsx
+++ components/ui/myFiles/listView.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function ListView({ list }: { list: any[] }) {
-  const router = useRouter();
+  console.log(list)
   return (
     <>
       <div className="grid gap-4 grid-cols-4 w-full">
         {list.map((item, index) => {
           return (
-            <button
-              // onClick={() => {if (tem?.category_name) router.push()}} Todo: add the url for the page you are making
+            <Link
+              href={`/dashboard/files/flashcards/${item.id}`}
               className="flex px-6 py-4 border rounded-xl border-black flex-col text-left overflow-hidden"
               key={index}
             >
@@ -20,7 +20,7 @@ export default function ListView({ list }: { list: any[] }) {
               {/* <span className="flex text-[0.875rem] text-nowrap w-48">
                 Total Characters: 4857
               </span> */}
-            </button>
+            </Link>
           );
         })}
       </div>
