133b4f752addd67bd50e615e95dcd13cb350ecee
Peter <obidaberechukwu@gmail.com>
2024-04-18 13:43:26 +0100

    worked on the flash cards stuff

--- components/ui/myFiles/listView.tsx
+++ components/ui/myFiles/listView.tsx
@@ -1,19 +1,25 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function ListView({ list }: { list: any[] }) {
+  const router = useRouter();
   return (
     <>
       <div className="grid gap-4 grid-cols-4 w-full">
         {list.map((item, index) => {
           return (
             <button
-              className="flex px-6 py-4 border rounded-xl border-black flex-col text-left"
+              // onClick={() => {if (tem?.category_name) router.push()}} Todo: add the url for the page you are making
+              className="flex px-6 py-4 border rounded-xl border-black flex-col text-left overflow-hidden"
               key={index}
             >
-              <span className="flex font-semibold text-nowrap w-48 line-clamp-1">
-                Thermodynamics Fluid Mechanics World Pollution
+              <span className="flex font-semibold w-48 line-clamp-2">
+                {item?.extracted_text?.[0] || item?.category_name}
               </span>
-              <span className="flex text-[0.875rem] text-nowrap w-48">
+              {/* <span className="flex text-[0.875rem] text-nowrap w-48">
                 Total Characters: 4857
-              </span>
+              </span> */}
             </button>
           );
         })}
